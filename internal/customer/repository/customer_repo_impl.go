b3b2439bf5b726d958501b12e76effcd1d5636ad
Tobi Albertino <muhamad_tobiaja232@apps.ipb.ac.id>
2023-06-03 20:42:29 +0700

    feat: repo impl add customer

--- internal/customer/repository/customer_repo_impl.go
+++ internal/customer/repository/customer_repo_impl.go
@@ -12,26 +12,46 @@ func NewCustomerRepository() CustomerRepository {
 }
 
 // CreateCustomer implements CustomerRepository.
-func (*CustomerRepositoryImpl) CreateCustomer(tx *sql.Tx, et entity.Customer) (int64, error) {
-	panic("unimplemented")
+func (repo *CustomerRepositoryImpl) CreateCustomer(tx *sql.Tx, et entity.Customer) (int64, error) {
+	SQL := `
+	INSERT INTO customers(first_name, last_name, email, avatar) 
+	VALUES (?, ?, ?, ?)`
+	varArgs := []interface{}{
+		et.FirstName,
+		et.LastName,
+		et.Email,
+		et.Avatar,
+	}
+
+	result, err := tx.Exec(SQL, varArgs...)
+	if err != nil {
+		return 0, err
+	}
+
+	i, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return i, nil
 }
 
 // DeleteCustomerByID implements CustomerRepository.
-func (*CustomerRepositoryImpl) DeleteCustomerByID(tx *sql.Tx, et entity.Customer) (int64, error) {
+func (repo *CustomerRepositoryImpl) DeleteCustomerByID(tx *sql.Tx, et entity.Customer) (int64, error) {
 	panic("unimplemented")
 }
 
 // GetAllCustomer implements CustomerRepository.
-func (*CustomerRepositoryImpl) GetAllCustomer(tx *sql.Tx, et entity.Customer) ([]entity.Customer, error) {
+func (repo *CustomerRepositoryImpl) GetAllCustomer(tx *sql.Tx, et entity.Customer) ([]entity.Customer, error) {
 	panic("unimplemented")
 }
 
 // GetCustomerByID implements CustomerRepository.
-func (*CustomerRepositoryImpl) GetCustomerByID(tx *sql.Tx, et entity.Customer) (entity.Customer, error) {
+func (repo *CustomerRepositoryImpl) GetCustomerByID(tx *sql.Tx, et entity.Customer) (entity.Customer, error) {
 	panic("unimplemented")
 }
 
 // UpdateCustomerByID implements CustomerRepository.
-func (*CustomerRepositoryImpl) UpdateCustomerByID(tx *sql.Tx, et entity.Customer) (int64, error) {
+func (repo *CustomerRepositoryImpl) UpdateCustomerByID(tx *sql.Tx, et entity.Customer) (int64, error) {
 	panic("unimplemented")
 }
